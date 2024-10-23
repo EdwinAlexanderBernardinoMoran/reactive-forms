585144bcca631f07c40b6ef4eac53f77c2c96bf5
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2024-10-22 22:23:17 -0600

    Getting started with reactive forms

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -6,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class BasicPageComponent {
 
+  // Manera 1
+  // public myform: FormGroup = new FormGroup({
+  //   name: new FormControl('', [], []),
+  //   price: new FormControl(0, [], []),
+  //   inStorage: new FormControl(0, [], []),
+  // })
+
+  // Form builder manera 2
+
+  private fb: FormBuilder = new FormBuilder();
+
+  public myForm: FormGroup = this.fb.group({
+    name: [''],
+    price: [0],
+    inStorage: [0],
+  });
+
+  onSave():void {
+    console.log(this.myForm.value);
+
+  }
 }
